e5f84e8b523c9f6cc8cd2d56c7489685130906e3
KrunalLathiya <krunallathiya21@gmail.com>
2017-07-19 19:25:07 +0530

    Middleware example complete

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ADMIN PAGE</title>
+  </head>
+  <body>
+    WELCOME TO ADMIN ROUTE
+  </body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  @if(\Session::has('error'))
+    <div class="alert alert-danger">
+      {{\Session::get('error')}}
+    </div>
+  @endif
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    <a href="{{url('admin/routes')}}">Admin</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
